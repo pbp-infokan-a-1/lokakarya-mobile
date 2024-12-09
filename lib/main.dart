d7126702ddcca1ba36a4271f0901591beb4c8675
Dermada <141832231+Dermada@users.noreply.github.com>
2024-12-09 14:31:17 +0700

    Benerin link homepage

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.deepPurple,
           ).copyWith(secondary: Colors.deepPurple[400]),
         ),
-        home: AdminDashboard(),
+        home: MyHomePage(),
       ),
     );
   }
